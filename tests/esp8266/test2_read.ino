5eeecfbde25ef42b6ca3e249e907058df90f8815
Philibert Cheminot <philibert.cheminot@gmail.com>
2021-08-07 19:11:59 +0200

    Correcting errors
    Tests are working.

--- tests/esp8266/test2_read.ino
+++ tests/esp8266/test2_read.ino
@@ -1,6 +1,8 @@
 #include <SoftwareSerial.h>
 SoftwareSerial StoveSerial;
 
+#define ENABLE_RX D2
+
 //This test prints the stove's state on serial monitor.
 
 #define stoveStatus 0x21
@@ -10,11 +12,12 @@ SoftwareSerial StoveSerial;
 #define ambTemp 0x01
 #define fumeTemp 0x5A
 #define stovePower 0x34
-uint8_t tempAmbient, tempFumes, stoveState;
+uint8_t stoveState, tempAmbient, tempFumes, flamePower;
 char stoveRxData[2];
 
 void checkStoveReply() //Works only when request is RAM
 {
+    delay(20);
     uint8_t rxCount = 0;
     stoveRxData[0] = 0x00;
     stoveRxData[1] = 0x00;
@@ -23,12 +26,13 @@ void checkStoveReply() //Works only when request is RAM
         stoveRxData[rxCount] = StoveSerial.read();
         rxCount++;
     }
+    digitalWrite(ENABLE_RX, HIGH);
     if (rxCount == 2)
     {
         byte val = stoveRxData[1];
         byte checksum = stoveRxData[0];
         byte param = checksum - val;
-        Serial.printf("Param=%01x value=%01x", param, value)
+        Serial.printf("Param=%01x value=%01x", param, val);
         switch (param)
         { 
         case stoveStatus:
@@ -43,6 +47,10 @@ void checkStoveReply() //Works only when request is RAM
             tempFumes = val;
             Serial.printf("T. fumes %d\n", tempFumes);
             break;
+        case stovePower:
+            flamePower = val;
+            Serial.printf("T. fumes %d\n", flamePower);
+            break;
         }
     }
 }
@@ -50,49 +58,66 @@ void checkStoveReply() //Works only when request is RAM
 void getStoveState()
 {
     const byte readByte = 0x00;
-    StoveSerial.enableTx(true);
     StoveSerial.write(readByte);
     delay(1);
     StoveSerial.write(stoveStatus);
-    StoveSerial.enableTx(false);
+    digitalWrite(ENABLE_RX, LOW);
+    delay(60);
     checkStoveReply();
+}
+
+void getStovePower()
+{
+    const byte readByte = 0x00;
+    StoveSerial.write(readByte);
+    delay(1);
+    StoveSerial.write(stovePower);
+    digitalWrite(ENABLE_RX, LOW);
+    delay(60);
+    checkStoveReply();
+    
 }
 
 void getAmbTemp()
 {
     const byte readByte = 0x00;
-    StoveSerial.enableTx(true);
     StoveSerial.write(readByte);
     delay(1);
     StoveSerial.write(ambTemp);
-    StoveSerial.enableTx(false);
+    digitalWrite(ENABLE_RX, LOW);
+    delay(60);
     checkStoveReply();
 }
 
 void getFumeTemp()
 {
     const byte readByte = 0x00;
-    StoveSerial.enableTx(true);
     StoveSerial.write(readByte);
     delay(1);
     StoveSerial.write(fumeTemp);
-    StoveSerial.enableTx(false);
+    digitalWrite(ENABLE_RX, LOW);
+    delay(60);
     checkStoveReply();
 }
 
 void getStates()
 {
+  
     getStoveState();
-    delay(150);
+    delay(100);
+    getStovePower();
+    delay(100);
     getAmbTemp();
-    delay(150);
+    delay(100);
     getFumeTemp();
 }
 
 void setup()
 {
+    pinMode(ENABLE_RX, OUTPUT);
+    digitalWrite(ENABLE_RX, HIGH); //The led of the optocoupler is off
     Serial.begin(115200);
-    StoveSerial.begin(1200, SWSERIAL_8N2, D4, D4, false, 256);
+    StoveSerial.begin(1200, SWSERIAL_8N2, D3, D4, false, 256);
     delay(2000);
     getStates();
 }
